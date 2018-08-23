77bef4affbb07483333ee494ed25581e99bee125
redcyb <redcyb@gmail.com>
2018-08-23 15:28:18 +0300

    update

--- vizualize_details.py
+++ vizualize_details.py
@@ -1,16 +1,15 @@
 import json
 import matplotlib.pyplot as plt
 
-with open("./details/rbm___hid_64___ep_10", "rb") as f:
+with open("./details/rbm___hid_64___ep_50", "rb") as f:
     rbm_results = json.loads(f.read())
 
-with open("./details/frbm___hid_64___ep_10", "rb") as ff:
+with open("./details/frbm___hid_64___ep_50", "rb") as ff:
     frbm_results = json.loads(ff.read())
 
 rbm_errors = rbm_results.get("errors")
 frbm_errors = frbm_results.get("errors")
 
-plt.plot(list(range(len(rbm_errors))), rbm_errors)
-# plt.clf()
-plt.plot(list(range(len(rbm_errors))), frbm_errors)
+plt.plot(list(range(len(rbm_errors))), rbm_errors, "b")
+plt.plot(list(range(len(rbm_errors))), frbm_errors, "r")
 plt.show()
